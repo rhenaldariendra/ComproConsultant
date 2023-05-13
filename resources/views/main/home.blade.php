35693eca77864d85e1c43da7e9292c5de9cca4e0
Rhenald Ariendra <54737784+rhenaldariendra@users.noreply.github.com>
2023-05-13 16:32:53 +0700

    Move Contact Us form to home & implement gmaps

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -2,12 +2,11 @@
 @section('title', 'Home')
 @section('content')
 <link rel="stylesheet" href="/assets/css/main/home.css">
+<link rel="stylesheet" href="/assets/css/main/contactus.css">
 
 <div class="content-1">
-    <p>Image Slider</p>
     <div class="container-item">
-        <div class="top"></div>
-        <div class="bottom"></div>
+        <p>Image Slider</p>
     </div>
 </div>
 
@@ -43,7 +42,8 @@
     </div>
     <div class="right">
         <h2>We Provide You The Best Services</h2>
-        <p>With our attention to detail and our up-to date knowledge on current laws and regulations, we thrive to deliver accurate calculations and solutions, relevant to your needs.</p>
+        <p>With our attention to detail and our up-to date knowledge on current laws and regulations, we thrive to
+            deliver accurate calculations and solutions, relevant to your needs.</p>
         <a href="#">Explore More <i class="bi bi-arrow-right-circle"></i></a>
     </div>
 </div>
@@ -71,5 +71,76 @@
     </div>
 </div>
 
+<div class="allbody">
+    <div class="container-top">
+        <div class="title-top">
+            <h2>30 minutes consultation</h2>
+            <p>Please fill in the form below</p>
+        </div>
+    </div>
+    <div class="container-bottom">
+        <div class="left-box">
+            <form action="">
+                <div class="top-container">
+                    <div class="input-placeholder">
+                        <label for="fname">Name</label>
+                        <input type="text" id="fname" name="fname" value="">
+                    </div>
+                    <div class="input-placeholder">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" value="">
+                    </div>
+                </div>
+                <div class="input-placeholder">
+                    <label for="service">Services Needed</label>
+                    <input type="text" id="service" name="service" value="">
+                </div>
+                <div class="input-placeholder">
+                    <label for="number">Whatsapp</label>
+                    <input type="text" id="number" name="number" value="">
+                </div>
+                <div class="input-placeholder">
+                    <label for="cname">Company Name</label>
+                    <input type="text" id="cname" name="cname" value="">
+                </div>
+                <div class="input-placeholder">
+                    <label for="msg">Message</label>
+                    <textarea name="msg" id="msg" cols="30" rows="10"></textarea>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
 
+        <div class="right-box">
+            <div class="mapouter">
+                <div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no"
+                        marginheight="0" marginwidth="0"
+                        src="https://maps.google.com/maps?width=600&amp;height=472&amp;hl=en&amp;q=gerobak kopi bang raden galaxy&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a
+                        href="https://capcuttemplate.org/">Capcut Templates</a></div>
+                <style>
+                    .mapouter {
+                        position: relative;
+                        text-align: right;
+                        width: 100%;
+                        height: 472px;
+                        border-radius: 10px;
+                    }
+
+                    .gmap_canvas {
+                        border-radius: 10px;
+                        overflow: hidden;
+                        background: none !important;
+                        width: 100%;
+                        height: 472px;
+                    }
+
+                    .gmap_iframe {
+                        height: 472px !important;
+                    }
+
+                </style>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
