5b50b1899fb2689eaa6766c52b40aafd834d34a7
AdyatmaRA <54709331+AdyatmaRA@users.noreply.github.com>
2023-05-29 20:57:05 +0700

    update pricing & home

--- resources/views/main/pricing.blade.php
+++ resources/views/main/pricing.blade.php
@@ -13,36 +13,36 @@
         <div class="box1">
             <div class="content_box">
                 <h1>Book Keeping</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                <p>We can assist in the licensing and registration process, be it for business permits, location permits, operational permits, or any other specific permits that may be required based on the type of business to be established.</p>
                  
                 <div class="btn_get">
                     <a href="#">get started</a>
                 </div>
                 <div class="box-desc">
-                        <p><i class="bi bi-check-circle-fill"></i> Record daily transaction</p>
+                        <p><i class="bi bi-check-circle-fill"></i>  Record daily transaction</p>
 
                         <div class="line"></div>
 
-                        <p><i class="bi bi-check-circle-fill"></i> Raising sales invoice</p>
+                        <p><i class="bi bi-check-circle-fill"></i>  Raising sales invoice</p>
 
                         <div class="line"></div>
 
-                        <p><i class="bi bi-check-circle-fill"></i> Booked purchased invoice</p>
+                        <p><i class="bi bi-check-circle-fill"></i>  Booked purchased invoice</p>
                         <div class="line"></div>
 
-                        <p><i class="bi bi-check-circle-fill"></i> Check PO,DO,invoice</p>
-
-                        <div class="line"></div>
-
-                        <p><i class="bi bi-check-circle-fill"></i> Payroll processing</p>
+                        <p><i class="bi bi-check-circle-fill"></i>  Check PO,DO,invoice</p>
 
                         <div class="line"></div>
 
-                        <p><i class="bi bi-check-circle-fill"></i> Bank reconciiation</p>
+                        <p><i class="bi bi-check-circle-fill"></i>  Payroll processing</p>
 
                         <div class="line"></div>
 
-                        <p><i class="bi bi-check-circle-fill"></i> Record receivable collections and deposits</p>
+                        <p><i class="bi bi-check-circle-fill"></i>  Bank reconciiation</p>
+
+                        <div class="line"></div>
+
+                        <p><i class="bi bi-check-circle-fill"></i>  Record receivable collections and deposits</p>
 
                         <div class="line"></div>
 
@@ -53,26 +53,24 @@
             </div>
         </div>
         
-        <div class="box1">
+        <div class="box2">
             <div class="content_box">
                 <h1>Tax Services</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+                <p>We offer in-depth knowledge of tax systems and complex tax regulations.</p>
                  
-                <div class="btn_get">
-                    <a href="#">get started</a>
+                <div class="btn_get_2">
+                    <a href="#"> get started</a>
                 </div>
-                <div class="box-desc">
-                        <p><i class="bi bi-check-circle-fill"></i> Monthly value added tax</p>
+                <div class="box-desc-2">
+                        <p><i class="bi bi-check-circle-fill"></i>  Monthly value added tax</p>
 
                         <div class="line"></div>
 
-                        <p><i class="bi bi-check-circle-fill"></i> Monthly income tax reporting</p>
+                        <p><i class="bi bi-check-circle-fill"></i>  Monthly income tax reporting</p>
 
                         <div class="line"></div>
 
-                        <p><i class="bi bi-check-circle-fill"></i> Annual tax reporting</p>
-
-                        <div class="line"></div>
+                        <p><i class="bi bi-check-circle-fill"></i>  Annual tax reporting</p>
                 </div>
                 
             </div>
